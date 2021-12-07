612b59bbd1090e751eb18b1cea4b8fc5c47e9fba
Burtscher Marcel (LWN) <Marcel.Burtscher@liebherr.com>
2021-12-07 13:26:19 +0100

    Day7

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -4,7 +4,7 @@ var sw = System.Diagnostics.Stopwatch.StartNew();
 
 //AdventOfCode.Day10Year2020.Main();
 
-Day6.Main();
+Day7.Main();
 
 sw.Stop();
 Console.WriteLine($"Time elapsed: {sw.ElapsedMilliseconds} ms");
---
+++ AdventOfCode/Year2021/Day7.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Year2021
+{
+    internal class Day7
+    {
+        internal static void Main()
+        {
+            var input = File.ReadAllLines("Year2021/Day7_Input.txt").First().Split(',').Select(x => Convert.ToInt32(x)).ToList();
+
+            #region Part1
+            //var positions = input.GroupBy(x => x).Select(x => new { x.Key, Count = x.Count() }).ToList();
+
+            //int min = positions.Min(x => x.Key);
+            //int max = positions.Max(x => x.Key);
+
+            //var array = new int[max - min+1];
+            //for (int i = min; i <= max; i++)
+            //{
+            //    var fuel = 0;
+            //    foreach (var position in positions)
+            //    {
+            //        if (position.Key == i)
+            //        {
+            //            continue;
+            //        }
+
+            //        if (position.Key < i)
+            //        {
+            //            fuel += (i - position.Key) * position.Count;
+            //        }
+            //        else
+            //        {
+            //            fuel += (position.Key - i) * position.Count;
+            //        }
+            //    }
+            //    array[i - min] = fuel;
+            //}
+
+            //var minFuel = array.Min();
+            //Console.WriteLine($"Fuel required: {minFuel}");
+            #endregion
+
+            #region Part2
+            var positions = input.GroupBy(x => x).Select(x => new { x.Key, Count = x.Count() }).ToList();
+
+            int min = positions.Min(x => x.Key);
+            int max = positions.Max(x => x.Key);
+
+            var array = new long[max - min + 1];
+            for (int i = min; i <= max; i++)
+            {
+                var fuel = 0l;
+                foreach (var position in positions)
+                {
+                    if (position.Key == i)
+                    {
+                        continue;
+                    }
+
+                    int steps;
+                    if (position.Key < i)
+                    {
+                        steps = i - position.Key;
+                    }
+                    else
+                    {
+                        steps = position.Key - i;
+                    }
+
+                    var answer = (steps * (steps + 1)) / 2;
+
+                    fuel += answer * position.Count;
+                }
+                array[i - min] = fuel;
+            }
+
+            var minFuel = array.Min();
+            Console.WriteLine($"Fuel required: {minFuel}");
+            #endregion
+        }
+    }
+}
